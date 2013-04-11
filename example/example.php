57695113f3eb9738cd1e7dacc0f20a3df858d632
qwerty23 <icefire23@gmail.com>
2013-04-11 13:19:44 +0900

    Update example.php

--- example/example.php
+++ example/example.php
@@ -6,5 +6,6 @@ $imgtext = new IMGText;
 $imgtext->cache_url_prefix = '.';
 $imgtext->cache_local_dir = dirname(__FILE__);
 $imgtext->font_dir = '/usr/share/fonts/truetype/nanum';
+$imgtext->set_shadow_effect("shadow", 5, 2);
 
 echo $imgtext->get_html("네이버 '나눔손글씨 붓'으로 작성된 제목", 'NanumBrush', 24, '#606060');
