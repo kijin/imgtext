daf62250f9efdfbfd2c9cae2193653e747239deb
Kijin Sung <kijin.sung@gmail.com>
2013-04-13 22:31:07 +0900

    Update example to use new features

--- example/example.md
+++ example/example.md
@@ -3,8 +3,8 @@ Example
 
 This is an example of printing a sentence of Korean using the open-source Nanum Brush font.
 
-<img class="imgtext" src="./imgtext.70da76625ab8.word-001.png" alt="네이버" title="" />&nbsp;
-<img class="imgtext" src="./imgtext.70da76625ab8.word-002.png" alt="&#039;나눔손글씨" title="" />&nbsp;
-<img class="imgtext" src="./imgtext.70da76625ab8.word-003.png" alt="붓&#039;으로" title="" />&nbsp;
-<img class="imgtext" src="./imgtext.70da76625ab8.word-004.png" alt="작성된" title="" />&nbsp;
-<img class="imgtext" src="./imgtext.70da76625ab8.word-005.png" alt="제목" title="" />&nbsp;
+<img class="imgtext" src="./imgtext.4b470a0626e7ca.word-001.png" alt="네이버" title="" /> &nbsp;
+<img class="imgtext" src="./imgtext.4b470a0626e7ca.word-002.png" alt="‘나눔손글씨" title="" /> &nbsp;
+<img class="imgtext" src="./imgtext.4b470a0626e7ca.word-003.png" alt="붓’으로" title="" /> &nbsp;
+<img class="imgtext" src="./imgtext.4b470a0626e7ca.word-004.png" alt="작성된" title="" /> &nbsp;
+<img class="imgtext" src="./imgtext.4b470a0626e7ca.word-005.png" alt="제목" title="" /> &nbsp;
--- example/example.php
+++ example/example.php
@@ -2,10 +2,30 @@
 
 // This is an example of printing a sentence of Korean using the open-source Nanum Brush font.
 
+include '../imgtext.php';
 $imgtext = new IMGText;
 $imgtext->cache_url_prefix = '.';
 $imgtext->cache_local_dir = dirname(__FILE__);
-$imgtext->font_dir = '/usr/share/fonts/truetype/nanum';
-$imgtext->set_shadow_effect("shadow", 5, 2);
 
-echo $imgtext->get_html("네이버 '나눔손글씨 붓'으로 작성된 제목", 'NanumBrush', 24, '#606060');
+// Font settings.
+
+$imgtext->font_dir = '/usr/share/fonts/truetype/nanum';
+$imgtext->font_name = 'NanumBrush';
+$imgtext->font_size = 32;
+$imgtext->color = '#404040';
+
+// Shadow settings.
+
+$imgtext->shadow = true;
+$imgtext->shadow_offset = array(2, 2);      // Horizontal, vertical.
+$imgtext->shadow_opacity = 64;              // 0 = Opaque, 127 = Transparent.
+$imgtext->shadow_color = '#a0a0a0';
+$imgtext->shadow_blur = 2;
+
+// Padding settings. Use this if your font gets cut at the edge.
+
+$imgtext->padding = array(0, 0, 0, 0);      // CSS-style: top-right-bottom-left.
+
+// Generate the HTML.
+
+echo $imgtext->get_html("네이버 ‘나눔손글씨 붓’으로 작성된 제목");
